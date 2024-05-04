061bb39131d74ebe14ec8e6a4807704918f47531
LeraPetrovich <valeria.petrovich@northitgroup.eu>
2024-05-04 22:55:02 +0300

    test

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,13 +1,12 @@
 import React, { useEffect, useState } from "react";
 import "./Form.css";
-// import { useTelegram } from "../../hooks/useTelegram";
-const tg = window.Telegram.WebApp;
+import { useTelegram } from "../../hooks/useTelegram";
 
 const Form = () => {
   const [country, setCountry] = useState("");
   const [city, setCity] = useState("");
   const [subject, setSubject] = useState("physical");
-//   const { tg } = useTelegram();
+  const { tg } = useTelegram();
 
   const onChangeCountry = (e) => {
     setCountry(e.target.value);
@@ -36,6 +35,7 @@ const Form = () => {
   return (
     <div className="form">
       <h3>Введите ваши данные</h3>
+      <p>{tg.MainButton}</p>
       <input
         className="input"
         type="text"
